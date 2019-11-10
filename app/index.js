340627b8bd815cebdcba41ca1d799c68f4f8047a
gschober <gschober@asu.edu>
2019-11-09 20:57:48 -0500

    Initial Commit
    Forgot to initial commit when I created the project...

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+import Popular from './components/Popular'
+
+//Components
+//-State
+//-Lifecycle
+//-UI
+
+class App extends React.Component{
+    render(){
+        return (
+            <div className='container'>
+                <Popular />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,//React Element
+    document.getElementById('app')//Where to render the element to
+)
